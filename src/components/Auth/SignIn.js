122c2e7050eda76e56dbf9cf05a657d9ccbff783
Coderc <dev@coderc.in>
2021-02-07 20:07:07 +0530

    Designed Sign In Form

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 export class SignIn extends Component {
     render() {
         return (
-            <div>
-                <h1>Sign In</h1>
+            <div className="signin_form container">
+                <h1 className="mb-5">Sign In</h1>
 
-                <Link to="/signup">Sign Up</Link>
+                <form className="row g-3 signin_form__container">
+                    <div className="col">
+                        <label for="username" className="form-label">
+                            Username
+                        </label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="username"
+                        />
+                    </div>
+
+                    <div className="col">
+                        <label for="inputPassword" className="form-label">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="inputPassword"
+                        />
+                    </div>
+                </form>
+
+                <div className="signin_button mt-5">Sign In</div>
             </div>
         );
     }
