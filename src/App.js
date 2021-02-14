c5191eb2568c34e3e3b3a73ca2e2a94fa1ffbe70
Coderc <dev@coderc.in>
2021-02-14 16:51:56 +0530

    Designed Sign In and Sign Up pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-    return <div></div>;
-}
+import React from 'react';
+import SignIn from './components/SignIn';
+
+const App = () => {
+    return (
+        <div className="App__container">
+            <SignIn />
+        </div>
+    );
+};
 
 export default App;
